2928494309a18b0deccfa5c96c95f4907516d5b7
Daniel Luo <d.luo117@gmail.com>
2025-08-13 17:05:29 -0700

    Initial Commit

---
+++ Cadence DJ/Cadence_DJApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cadence_DJApp.swift
+//  Cadence DJ
+//
+//  Created by Daniel Luo on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Cadence_DJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cadence DJTests/Cadence_DJTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cadence_DJTests.swift
+//  Cadence DJTests
+//
+//  Created by Daniel Luo on 8/13/25.
+//
+
+import Testing
+@testable import Cadence_DJ
+
+struct Cadence_DJTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
