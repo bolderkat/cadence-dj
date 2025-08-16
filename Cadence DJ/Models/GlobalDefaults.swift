da564dcbaaf7306053607ac08091354d70d27e22
Daniel Luo <d.luo117@gmail.com>
2025-08-15 17:28:39 -0700

    Add basic navigation menu

---
+++ Cadence DJ/Models/GlobalDefaults.swift
@@ -0,0 +1,11 @@
+//
+//  GlobalDefaults.swift
+//  Cadence DJ
+//
+//  Created by Daniel Luo on 8/15/25.
+//
+
+enum GlobalDefaults {
+    static let defaultCadence = 170
+    static let cadenceRange = 100...250
+}
